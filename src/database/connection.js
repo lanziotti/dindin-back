43fbf6516d798a0f47e4d46fe1d3267f86cae09f
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-12-02 10:49:56 -0300

    Inserindo o url para deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,6 +8,7 @@ const knex = require('knex')({
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE,
+        URL: process.env.DB_URI,
         ssl: {rejectUnauthorized: false}
     }
 });
