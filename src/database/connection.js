8bad731cfa492049871f3f0d48c0d9642de6e7bc
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-12-02 11:24:11 -0300

    Correção connection 2

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,11 +3,6 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE,
         URL: process.env.DB_URI,
         ssl: {rejectUnauthorized: false}
     }
