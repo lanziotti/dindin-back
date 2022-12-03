110500289c5e2a24db10f6edb9b6e484278ca474
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-12-02 21:26:16 -0300

    Correção deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        URL: process.env.DB_URI,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE,
         ssl: {rejectUnauthorized: false}
     }
 });
